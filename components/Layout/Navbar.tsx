6f060110f50435decfb6f24e238d9f11295006da
Pdsadhura <dhimanparas23@gmail.com>
2026-06-26 02:00:01 +0530

    mahadev clone 5

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  const whatsappUrl = "https://wa.me/918872189471";
+  const whatsappUrl = "https://wa.me/917589045547";
 
   return (
     <header className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
